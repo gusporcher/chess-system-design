5e8cbdacf93f6ce7309db9402df6837950c24581
= <=>
2019-11-07 17:09:51 -0200

    placing pieces on board

---
+++ src/chess/pieces/Rock.java
@@ -0,0 +1,20 @@
+package chess.pieces;
+
+import bordgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rock extends ChessPiece{
+
+	public Rock(Board board, Color color) {
+		super(board, color);		
+	}
+
+	@Override
+	public String toString() {
+		return "R";
+	}
+	
+	
+	
+}
